76d50a42f8819e69b346b028b8a2ae190a9bfe52
thunrobi <thunrobi@arcada.fi>
2025-02-11 11:53:13 +0200

    Lektion 11.2

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,21 @@
 public class Person {
+
     String name;
+    int rank;
+
     public Person(String name) {
         this.name = name;
     }
-    /*public Person(String name, Person next) {
+    public Person(String name, int rank) {
         this.name = name;
-        this.next = next;
-
-    } */
+        this.rank = rank;
+    }
 
     public String getName() {
         return name;
     }
-
+    public int getRank(){
+        return rank;
+    }
 
 }
